763e71a73b514903bb64f65e4fb86643efb02a0f
Asim Poptani <asim@poptani.com>
2023-12-28 10:25:10 +0000

    Trying 32x32

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ class LayerNormHelper(L.LightningModule):
         return torch.nn.LayerNorm(x.shape[1:])(x)
 
 class Generator(L.LightningModule):
-    def __init__(self, depth=1, noise_size=(4,4), noise_channels=1):
+    def __init__(self, depth=1, noise_size=(8,8), noise_channels=1):
         super().__init__()
         self.noise_size=noise_size
         self.noise_channels=noise_channels
         self.depth=depth
         # 3x32x32 of gaussian noise
         self.initial=torch.nn.Sequential(
-            torch.nn.ConvTranspose2d(in_channels=noise_channels, out_channels=64, kernel_size=3, stride=1),
+            torch.nn.ConvTranspose2d(in_channels=noise_channels, out_channels=64, kernel_size=5, stride=1),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64)
         )
@@ -44,11 +44,11 @@ class Generator(L.LightningModule):
             torch.nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
             torch.nn.LeakyReLU(),
             torch.nn.BatchNorm2d(64)
-        ) for index in range(2)])
+        ) for index in range(4)])
         # +8
         # Convert to 32x32x3
         self.final=torch.nn.Sequential(
-            torch.nn.ConvTranspose2d(in_channels=64, out_channels=3, kernel_size=3),
+            torch.nn.ConvTranspose2d(in_channels=64, out_channels=3, kernel_size=5),
             # +2
             torch.nn.LeakyReLU(),
         )
@@ -220,7 +220,6 @@ if __name__ == "__main__":
 
     transforms=torchvision.transforms.Compose([
         torchvision.transforms.PILToTensor(),
-        torchvision.transforms.Resize((16, 16)),
         lambda x: x/255.0,
 
         # torchvision.transforms.Normalize(mean=(0.5,0.5,0.5),std=(0.5,0.5,0.5))
@@ -234,7 +233,7 @@ if __name__ == "__main__":
     trainer = L.Trainer(logger=[
         tensorboard_logger,
         wandb_logger
-    ], precision=16)
+    ])
     # Get one class from cifar100
     cifar100 = torch.utils.data.Subset(cifar100, [i for i in range(len(cifar100)) if cifar100[i][1] == 98])
     # Convert to Dataset
@@ -243,7 +242,7 @@ if __name__ == "__main__":
     cifar100 = torch.utils.data.DataLoader(cifar100, batch_size=100, shuffle=True, num_workers=9)
 
 
-    trainer.fit(ImprovedWassersteinGAN(Generator(depth=10), Discriminator(depth=10,image_size=(16,16))), cifar100)
+    trainer.fit(ImprovedWassersteinGAN(Generator(depth=10), Discriminator(depth=10,image_size=(32,32))), cifar100)
 
 
 
