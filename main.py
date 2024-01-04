dc888725415936e9410aba18a9602d8ceffd3896
Asim Poptani <asim@poptani.com>
2024-01-04 16:07:33 +0000

    Move to gpu

--- main.py
+++ main.py
@@ -230,8 +230,10 @@ if __name__ == "__main__":
 
     # Get one class from cifar100
     cifar100 = torch.utils.data.Subset(cifar100, [i for i in range(len(cifar100)) if cifar100[i][1] == 98])
-    # Convert to Dataset
-    cifar100 = torch.utils.data.TensorDataset(torch.stack([x[0] for x in cifar100]))
+
+    # Move to GPU
+    cifar100 = torch.utils.data.TensorDataset(torch.stack([x[0] for x in cifar100]).cuda())
+
     # Convert to dataloader
     cifar100 = torch.utils.data.DataLoader(cifar100, batch_size=500, shuffle=True, num_workers=8, pin_memory=True, persistent_workers=True)
     torch.set_float32_matmul_precision('medium')
